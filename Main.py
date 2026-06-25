e43eb92b11ba95d7fb61b1987c386a4963c709b5
AsroulAfizie <asroulafizie06@gmail.com>
2026-06-25 09:17:59 +0800

    Add Dictionaries

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ print(x)
 # squareNum = intNum ** 2
 # print(squareNum)
 
-age = 35
+age = 55
 text = "Ali is %d." % age
 print(text)
 
@@ -22,3 +22,42 @@ print(text)
 
 for x in range(6):
     print(x +1)
+
+def myFunction():
+    print("This is a fucntion without argument.")
+
+def multiplication(vName1, vName2):
+    return vName1 * vName2
+
+vSent1 = input("Please Enter first Number >>")
+vsent2 = input("please enter Second Number >>")
+
+print(multiplication(float(vSent1), float(vsent2)))
+myFunction()
+
+myCollection = {}
+myCollection[1] = "First To Die = Harith"
+myCollection[2] = "2nd Chance"
+myCollection[3] = "3rd Degree"
+
+myCollectionCross = {
+    "No.1" : "Along Came A spider",
+    "No.2" : "Kiss The Girls",
+    "No.3" : "Jack And Jill",
+    "No.4" : "Cat And Mouse"
+}
+
+testScores = {
+    "Mariam" : 78,
+    "Faridah" : 87,
+    "Rashid" : 65,
+    "Farid" : 80
+}
+
+myCollection[4] = "4th of July"
+myCollectionCross["No.5"] = "Pop Goes The Weasel"
+testScores["Amran"] = 45
+
+print (myCollectionCross)
+print(myCollection)
+print(testScores)
