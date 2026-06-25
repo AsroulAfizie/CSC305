1809efad5f64d803bb33c950ea0f058d5d274a60
AsroulAfizie <asroulafizie06@gmail.com>
2026-06-25 08:41:34 +0800

    Added Scanner in Python

---
+++ Main.py
@@ -0,0 +1,10 @@
+print("Hello World")
+
+print("Hello", "How are you?", sep="---")
+x = ("apple", "banana", "cherry")
+print(x)
+
+strNum = input("Enter a Number >> ")
+intNum = int(strNum)
+squareNum = intNum ** 2
+print(squareNum)
