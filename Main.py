bc1885087d0239e1ffa13f807a79fc3fc1b4afd9
AsroulAfizie <asroulafizie06@gmail.com>
2026-06-25 08:52:08 +0800

    Trying Python Syntax

--- Main.py
+++ Main.py
@@ -4,7 +4,18 @@ print("Hello", "How are you?", sep="---")
 x = ("apple", "banana", "cherry")
 print(x)
 
-strNum = input("Enter a Number >> ")
-intNum = int(strNum)
-squareNum = intNum ** 2
-print(squareNum)
+# strNum = input("Enter a Number >> ")
+# intNum = int(strNum)
+# squareNum = intNum ** 2
+# print(squareNum)
+
+age = 35
+text = "Ali is %d." % age
+print(text)
+
+myResult = 8%7
+print("Result is -> %d" %myResult)
+
+name = "John"
+text = "My Name is " + name + " and I am %d."% age
+print(text)
