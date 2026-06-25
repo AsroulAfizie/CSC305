28b44f0dea6bc635ae5761d64c21dc1d6eb237c3
AsroulAfizie <asroulafizie06@gmail.com>
2026-06-25 08:31:15 +0800

    Added Scanner in VSCode

--- GroupProjectJava/SeafoodSupplierSystem.java
+++ GroupProjectJava/SeafoodSupplierSystem.java
@@ -1,5 +1,14 @@
+import java.util.*;
+
 public class SeafoodSupplierSystem{
     public static void main(String [] args){
-        System.out.print("Hello World");
+        System.out.println("Hello World");
+
+        System.out.print("Enter A number : ");
+        Scanner scan = new Scanner(System.in);
+
+        int num = scan.nextInt();
+
+        System.out.println("Your Number " + num);
     }
 }
