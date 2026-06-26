693cb3e3306de80400ff2e93cf6e66083e073c85
Amir Asyraf <2024420514@student.uitm.edu.my>
2026-06-26 16:37:00 +0800

    NIGGA

--- GroupProjectJava/Main.java
+++ GroupProjectJava/Main.java
@@ -4,7 +4,7 @@ public class Main{
     public static void main(String [] args){
         System.out.println("Hello World");
 
-        System.out.print("Enter A number : ");
+        System.out.print("Enter A nigga name : ");
         Scanner scan = new Scanner(System.in);
 
         int num = scan.nextInt();
