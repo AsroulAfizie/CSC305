3657dbd98ad98aee5734fbd0fef494f975d2937a
AsroulAfizie <asroulafizie06@gmail.com>
2026-06-30 15:16:39 +0800

    Almost Completed System

--- GroupProjectJava/SeafoodOrder.java
+++ GroupProjectJava/SeafoodOrder.java
@@ -1,4 +1,56 @@
-public class SeafoodOrder {
-    private double totalDailyIncome;
+import java.util.ArrayList;
+
+public class SeafoodOrder extends Delivery {
+    ArrayList<String> seafoodName = new ArrayList<>();
+    ArrayList<Integer> seafoodQuantity = new ArrayList<>();
     
+    public SeafoodOrder(){
+        super();
+        seafoodName = null;
+        seafoodQuantity = null;
+    }
+
+    public SeafoodOrder(String n, String phone, String a, String d, ArrayList<String> sN, ArrayList<Integer> qtty){
+        super(n, phone, a, d);
+        seafoodName = new ArrayList<>(sN);
+        seafoodQuantity= new ArrayList<>(qtty);
+    }
+
+    public double calcTotalPrice(){
+        double totalPrice = 0;
+        for(int i = 0; i < seafoodName.size(); i++){
+            String seafood = seafoodName.get(i);
+            int qtty = seafoodQuantity.get(i);
+
+            if(seafood.equalsIgnoreCase("Tiger Shrimp")){
+                totalPrice += qtty * 200;
+            }
+            else if(seafood.equalsIgnoreCase("White Shrimp")){
+                totalPrice += qtty * 125;
+            }
+            else if(seafood.equalsIgnoreCase("Royal Red Shrimp")){
+                totalPrice += qtty * 175;
+            }
+            else if(seafood.equalsIgnoreCase("Blue Pincer Crab")){
+                totalPrice += qtty * 150;
+            }
+            else if(seafood.equalsIgnoreCase("King Crab")){
+                totalPrice += qtty * 990;
+            }
+        }
+
+        totalPrice += super.deliveryFee();
+        return totalPrice;
+    }
+
+    public String toString(){
+        StringBuilder data = new StringBuilder();
+
+        for(int i = 0; i < seafoodName.size(); i++){
+            data.append(seafoodName.get(i)).append(" : ").append(seafoodQuantity.get(i)).append("\n");
+        }
+
+        return super.toString() + data + calcTotalPrice();
+    }
+
 }
