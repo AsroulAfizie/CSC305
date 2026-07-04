79d0d9d06764c6c97abaad61c5f914a27a5203c8
AsroulAfizie <asroulafizie06@gmail.com>
2026-07-04 12:57:49 +0800

    Added Both Struct for For Seafood and Delivery

--- GroupProjectC/Main.C
+++ GroupProjectC/Main.C
@@ -1,6 +1,20 @@
 #include <stdio.h>
 //STARTING
+
+struct Seafood{
+    char seafoodName[100];
+    double seafoodPrice[100];
+};
+
+struct Delivery{
+    char name[100];
+    char phoneNum[100];
+    char address[100];
+    char deliveryType[100];
+    struct Seafood seafood;
+};
+
+
 int main(void) {
-	printf("Hello, world.\n");
-	return 0;
+    
 }
